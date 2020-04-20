e463a4a2745ee915d14fd34783fd0a11a0f91d0e
Jordan <jordan.wallace.williams@gmail.com>
2020-04-20 16:09:28 -0600

    Sessions controller

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -1,5 +1,14 @@
 class Api::V1::Users::SessionsController < ApplicationController
   def create
+    if user == User.authenticate(user_params[:email], params[:password])
+      session[:current_user] = user.id
+      redirect_to root_url
+    end
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,6 +1,5 @@
 require './app/models/location'
 require './app/models/forecast'
-require './app/models/pexels_image'
 
 class ForecastFacade
   attr_reader :location,
@@ -10,9 +9,11 @@ class ForecastFacade
 
   def initialize(location)
     @location = location
+
     @geo_service = GeocodeService.new
     @geo_location = @geo_service.find_location(location)
     @location_coordinates = Location.new(@geo_location)
+
     @weather_service = WeatherService.new
     @weather = Forecast.new(@weather_service.weather_data(@location_coordinates))
     @extended_forecast = ExtendedForecast.new(location, @weather.forecast)
