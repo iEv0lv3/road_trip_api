6d60b8daa5d244231ea45bd79044354c7a0ef2ad
Jordan <jordan.wallace.williams@gmail.com>
2020-04-20 21:02:22 -0600

    Remove unnecessary instance variable for api_key

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -4,7 +4,6 @@ class RoadTripFacade
   def initialize(trip_params)
     @origin = trip_params[:origin]
     @destination = trip_params[:destination]
-    @api_key = trip_params[:api_key]
     @trip_duration = MapsService.new.directions(@origin, @destination)
     @trip_forecast = ForecastFacade.new(@destination).weather
     @trip = RoadTrip.new(trip_params, @trip_duration, @trip_forecast)
