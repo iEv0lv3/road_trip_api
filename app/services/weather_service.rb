2eef20ed9de791139aaef9f3cbca492c6805fa2f
Jordan <jordan.wallace.williams@gmail.com>
2020-04-21 12:41:58 -0600

    Add low level caching to weather service

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,12 +1,14 @@
 class WeatherService
   def weather_data(location)
-    response = connection.get('/data/2.5/onecall') do |faraday|
-      faraday.params['lat'] = location.lat
-      faraday.params['lon'] = location.long
-      faraday.params['units'] = 'imperial'
+    Rails.cache.fetch(location, expires_in: 1.hours) do
+      @response = connection.get('/data/2.5/onecall') do |faraday|
+        faraday.params['lat'] = location.lat
+        faraday.params['lon'] = location.long
+        faraday.params['units'] = 'imperial'
+      end
     end
 
-    JSON.parse(response.body, symbolize_names: true)
+    JSON.parse(@response.body, symbolize_names: true)
   end
 
   private
