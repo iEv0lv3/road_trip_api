c7b99d981650ac7230f49b769d8036f770b28be9
Jordan <jordan.wallace.williams@gmail.com>
2020-04-21 12:35:02 -0600

    Add low leveling caching to pexels image service

--- app/services/pexels_service.rb
+++ app/services/pexels_service.rb
@@ -1,12 +1,14 @@
 class PexelsService
   def find_image(location)
-    response = connection.get("/v1/search?") do |faraday|
-      faraday.params['query'] = location
-      faraday.params['page'] = 1
-      faraday.params['per_page'] = 1
+    Rails.cache.fetch('recent_image', expires_in: 6.hours) do
+      @response = connection.get("/v1/search?") do |faraday|
+        faraday.params['query'] = location
+        faraday.params['page'] = 1
+        faraday.params['per_page'] = 1
+      end
     end
 
-    JSON.parse(response.body, symbolize_names: true)
+    JSON.parse(@response.body, symbolize_names: true)
   end
 
   private
